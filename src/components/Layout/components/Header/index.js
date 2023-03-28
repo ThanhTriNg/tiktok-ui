cf71a0c2561c6eecb3fd004b0f2105ca2581b687
Zan <74861824+nhaboyy@users.noreply.github.com>
2023-03-28 16:54:23 +0700

    Add files via upload

---
+++ src/components/Layout/components/Header/index.js
@@ -0,0 +1,14 @@
+//npm i classnames
+import classNames from 'classnames/bind';
+
+import styles from './Header.module.scss';
+const cx = classNames.bind(styles);
+function Header() {
+  return (
+    <header className={cx('wrapper')}>
+      <div className={cx('inner')}>OK </div>
+    </header>
+  );
+}
+
+export default Header;
